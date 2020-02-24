955c2354864c25cff4d358d2c382bbc5c8896696
DELMARKOS <markos.delaportas@gmail.com>
2020-02-25 00:03:22 +0200

    DesktopInitialCommit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+function changeBackground(pos) {
+    document.body.style.backgroundImage = "url('../img/"+pos+".png')";
+}
+function resetBackground() {
+    document.body.style.backgroundImage = "url('../img/initial.png')";
+}
