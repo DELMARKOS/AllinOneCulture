f2f60530523e803b4593d9d7cac6894686b57d6b
DELMARKOS <delaportas.markos@gmail.com>
2020-12-17 16:56:55 +0200

    added event to chenge h1 name

--- js/index.js
+++ js/index.js
@@ -1,3 +1,27 @@
+const nameCng = document.getElementById('aioc');
+
+nameCng.addEventListener('onSmallSize', function (e) {
+    nameCng.textContent = e.detail.textContent;//'AiOC';
+}, false);
+
+function updateText(name) {
+    if (window.innerWidth < 600) {
+        let event = new CustomEvent('onSmallSize', {
+            detail:{
+                textContent: "AiOC"
+            }
+        });
+        nameCng.dispatchEvent(event);
+    } else {
+        let event = new CustomEvent('onSmallSize', {
+            detail:{
+                textContent: "AllinOneCulture"
+            }
+        });
+        nameCng.dispatchEvent(event);
+    }
+}
+
 function changeBackground(pos) {
     document.body.style.backgroundImage = "url('../img/"+pos+".png')";
 }
