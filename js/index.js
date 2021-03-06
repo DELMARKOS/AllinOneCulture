9373e0ba8096fd0885592197ab657ae5700f3f75
DELMARKOS <delaportas.markos@gmail.com>
2021-03-06 17:57:59 +0200

    added some responsiveness

--- js/index.js
+++ js/index.js
@@ -1,30 +1,10 @@
-const nameCng = document.getElementById('aioc');
-
-//Todo make this work!
-nameCng.addEventListener('onSmallSize', function (e) {
-    nameCng.textContent = e.detail.textContent;//'AiOC';
-}, false);
-
-function updateText(name) {
-    if (window.innerWidth < 600) {
-        let event = new CustomEvent('onSmallSize', {
-            detail:{
-                textContent: "AiOC"
-            }
-        });
-        nameCng.dispatchEvent(event);
-    } else {
-        let event = new CustomEvent('onSmallSize', {
-            detail:{
-                textContent: "AllinOneCulture"
-            }
-        });
-        nameCng.dispatchEvent(event);
-    }
+function changeName() {
+    document.getElementById('aioc').innerHTML =
+        (document.body.scrollWidth < 900) ? 'AiOC' : 'AllinOneCulture';
 }
 
 function changeBackground(pos) {
-    document.body.style.backgroundImage = "url('../img/"+pos+".png')";
+    document.body.style.backgroundImage = "url('../img/" + pos + ".png')";
 }
 function resetBackground() {
     document.body.style.backgroundImage = "url('../img/initial.png')";
